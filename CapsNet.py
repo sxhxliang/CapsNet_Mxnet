c9034a13bec12136bfb61a489359f56800f325f3
AaronLeong <626786864@qq.com>
2017-11-28 18:53:54 +0800

    update version

--- CapsNet.py
+++ CapsNet.py
@@ -21,9 +21,9 @@ def CapsNet(batch_size, ctx):
     net = nn.Sequential()
     with net.name_scope():
 
-        net.add(nn.Conv2D(channels=256, kernel_size=9, strides=1, padding=(0,0), context=ctx, activation='relu'))
+        net.add(nn.Conv2D(channels=256, kernel_size=9, strides=1, padding=(0,0), activation='relu'))
         net.add(PrimaryConv(dim_vector=8, n_channels=32, kernel_size=9, strides=2, context=ctx, padding=(0,0)))
-        net.add(DigitCaps(num_capsule=10, dim_vector=16))
+        net.add(DigitCaps(num_capsule=10, dim_vector=16, context=ctx))
         net.add(Length())
 
     net.initialize(ctx=ctx, init=init.Xavier())
